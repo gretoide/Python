1a676f95d1b3ca7768232f1ae3b5fb726f9419ee
gretoide <gretaxpiacentini@gmail.com>
2023-04-01 19:19:08 -0300

    ✅

--- ejercicios/practica_2/ejercicio_8.py
+++ ejercicios/practica_2/ejercicio_8.py
@@ -1,1 +1,19 @@
-print('probando')
+# 8. Escriba un programa que solicite que se ingrese una palabra
+# o frase y permita identificar si la misma es un Heterograma 
+# (tenga en cuenta que el contenido del enlace es una traducción del inglés por 
+# lo cual las palabras que nombra no son heterogramas en español). 
+# Un Heterograma es una palabra o frase que no tiene 
+# ninguna letra repetida entre sus caracteres.
+
+def is_heterogram():
+    """
+    Nos indica si un texto es un heterograma (no tiene letras repetidas) o no
+    """
+    word = input('Ingrese una palabra: ')
+    letters = set(word)
+    if(len(word) == len(letters)):
+        print(f'Es un heterograma')
+    else:
+        print('No es un heterograma')
+
+is_heterogram()
